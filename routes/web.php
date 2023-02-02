41eaa5c682f1c95d45aa124118ad3747503dca67
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2023-02-02 16:22:39 +0100

    update web

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('home');
+// });
 Route::get('/', function () {
     return view('welcome');
 });
@@ -28,4 +31,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
