551d7082fd0865cdeb760f0543027f5d2fc17b70
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-02-06 00:19:03 +0100

    update web

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// Route::get('/', [HomeController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/test', function () {
     return view('test');
 });
