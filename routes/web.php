6faa451d872d60410ba5ddbb8665fbb6de1c156f
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2023-02-07 16:22:06 +0100

    fetch all meals from database into table

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MealController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,14 +23,18 @@ Route::get('/test', function () {
     return view('home');
 });
 
-Route::get('/meals', function () {
-    return view('meals');
-})->middleware(['auth', 'verified'])->name('meals');
+// Route::get('/meals', function () {
+//     return view('meals');
+// })->middleware(['auth', 'verified'])->name('meals');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/meals', [MealController::class, 'index'])->name('meals');
+    Route::get('/meals/edit/{slug}', [MealController::class, 'index'])->name('meals/edit');
+});
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
