3cd2952b5f688164ab9350ecaa512859b5c4d972
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-02-12 13:39:02 +0100

    add pagination tailwind into table

--- resources/views/meals/index.blade.php
+++ resources/views/meals/index.blade.php
@@ -66,7 +66,7 @@
                                     {{ $meal->created_at->diffForHumans() }}
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    {{ $meal->price }}
+                                    {{ $meal->price }} Dhs
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right">
 
@@ -84,29 +84,15 @@
                                 </td>
                             </tr>
                         @endforeach
-
-                        {{-- <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Magic Mouse 2
-                            </th>
-                            <td class="px-6 py-4">
-                                Black
-                            </td>
-                            <td class="px-6 py-4">
-                                Accessories
-                            </td>
-                            <td class="px-6 py-4">
-                                $99
-                            </td>
-                            <td class="px-6 py-4 text-right">
-                                <a href="#"
-                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr> --}}
                     </tbody>
                 </table>
             </div>
+            <div class="row">
+                <div class="w-full py-4">
+                    {{ $meals->links('pagination::tailwind') }}
+                </div>
+            </div>
+
 
         </div>
     </div>
